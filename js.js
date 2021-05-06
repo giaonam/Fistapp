35bea1f4cd41b4badd89b2a7d48ce776e72b582f
Nam Giao <namgpheasd@adf.com>
2021-05-06 18:08:26 +0700

    Lan dau lam chuyen ay

---
+++ js.js
@@ -0,0 +1,11 @@
+let arr = ["1", "2", "3","4","5"];
+let str = "mot hai ba bon";
+let text ="";
+
+text = add(2,"3");
+text = Number("asd");
+    document.getElementById("demo").innerHTML = text;
+
+    function add(a,b){       
+        return a/b;
+    }
